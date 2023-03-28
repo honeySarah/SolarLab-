c5201e41efc55767d3e8dd24fcc3569d99b59d2e
Lesokhina Alexandra <honeysarah@mail.ru>
2023-03-28 15:11:40 +0300

    Убрала лишние тэги и фалйлы

--- angularHomework/angularHomework/src/app/app.component.ts
+++ angularHomework/angularHomework/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { books as data} from './data/book';
-import { BooksService } from './data/books.service';
-import { IBook } from './interface/book';
 
 @Component({
   selector: 'app-root',
